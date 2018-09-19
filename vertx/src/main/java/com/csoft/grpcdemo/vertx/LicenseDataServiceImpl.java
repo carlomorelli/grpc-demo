f528e8d7073aa0b7529decc7bdbfd6ef799f7678
Carlo Morelli <carlomorelli@gmail.com>
2018-09-19 14:04:05 +0200

    Adding vert.x dedicated API

--- vertx/src/main/java/com/csoft/grpcdemo/vertx/LicenseDataServiceImpl.java
+++ vertx/src/main/java/com/csoft/grpcdemo/vertx/LicenseDataServiceImpl.java
@@ -6,13 +6,15 @@ import com.csoft.grpcdemo.licensedata.LicenseDataResponse;
 import com.csoft.grpcdemo.licensedata.LicenseDataServiceGrpc;
 import com.csoft.grpcdemo.licensedata.MyEnum;
 import io.grpc.stub.StreamObserver;
+import io.vertx.core.Future;
 
 import static java.lang.String.format;
 
-public class LicenseDataServiceImpl extends LicenseDataServiceGrpc.LicenseDataServiceImplBase {
+public class LicenseDataServiceImpl extends LicenseDataServiceGrpc.LicenseDataServiceVertxImplBase {
+
 
     @Override
-    public void fetchLicenseData(LicenseDataRequest request, StreamObserver<LicenseDataResponse> responseObserver) {
+    public void fetchLicenseData(final LicenseDataRequest request, final Future<LicenseDataResponse> futureResponse) {
 
         LicenseDataResponse response = LicenseDataResponse.newBuilder()
                 .setEnum(MyEnum.value0)
@@ -22,6 +24,31 @@ public class LicenseDataServiceImpl extends LicenseDataServiceGrpc.LicenseDataSe
                         request.getOther(),
                         request.getInternal().getName() + request.getInternal().getSurname()))
                 .build();
+
+        System.out.println("Getting request: " + request);
+        System.out.println("Answering: " + response);
+        futureResponse.complete(response);
+
+    }
+}
+
+
+/*
+// Alternative, using the traditional way:
+public class LicenseDataServiceImpl extends LicenseDataServiceGrpc.LicenseDataServiceImplBase {
+
+    @Override
+    public void fetchLicenseData(LicenseDataRequest request, StreamObserver<LicenseDataResponse> responseObserver) {
+
+        LicenseDataResponse response = LicenseDataResponse.newBuilder()
+            .setEnum(MyEnum.value0)
+            .setSomeValue(format("This is Vert.x. Answering to %s, %s, %s, %s",
+                request.getKeyId(),
+                request.getOptional(),
+                request.getOther(),
+                request.getInternal().getName() + request.getInternal().getSurname()))
+            .build();
+
         System.out.println("Getting request: " + request);
         System.out.println("Answering: " + response);
 
@@ -29,4 +56,5 @@ public class LicenseDataServiceImpl extends LicenseDataServiceGrpc.LicenseDataSe
         responseObserver.onCompleted();
 
     }
-}
+
+}*/
--- vertx/src/test/java/com/csoft/grpcdemo/vertx/ClientTest.java
+++ vertx/src/test/java/com/csoft/grpcdemo/vertx/ClientTest.java
@@ -3,9 +3,12 @@ package com.csoft.grpcdemo.vertx;
 import com.csoft.grpcdemo.licensedata.*;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
+import io.vertx.grpc.VertxChannelBuilder;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 
 import java.util.UUID;
 import java.util.concurrent.ExecutionException;
@@ -15,7 +18,7 @@ import java.util.concurrent.TimeoutException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class ClientTest {
 
     private ManagedChannel channel;
@@ -33,14 +36,14 @@ public class ClientTest {
     public void before() {
     }
 
-    @AfterEach
-    public void after() {
+    @AfterAll
+    public void afterAll() {
         System.out.println("Shutting down channel...");
         channel.shutdown();
     }
 
     @Test
-    public void testVertxServer() throws ExecutionException, InterruptedException, TimeoutException {
+    public void testVertxServerWithTraditionalClient() throws ExecutionException, InterruptedException, TimeoutException {
 
         channel = ManagedChannelBuilder.forAddress("localhost", 50001)
                 .usePlaintext(true)
@@ -51,7 +54,36 @@ public class ClientTest {
 
         Future<LicenseDataResponse> asyncResponse = asyncClient.fetchLicenseData(request);
 
-        assertEquals(MyEnum.value0, asyncResponse.get(2000, TimeUnit.MILLISECONDS).getEnum());
+        System.out.println("passed traditionalclient");
+        assertEquals(MyEnum.value0, asyncResponse.get(5000, TimeUnit.MILLISECONDS).getEnum());
 
     }
+
+
+
+    @Test
+    public void testVertxServerWithVertxClient() {
+
+        channel = VertxChannelBuilder.forAddress("localhost", 50001)
+            .usePlaintext(true)
+            .build();
+
+        LicenseDataServiceGrpc.LicenseDataServiceVertxStub asyncClient =
+            LicenseDataServiceGrpc.newVertxStub(channel);
+
+        asyncClient.fetchLicenseData(request, asyncResult -> {
+
+            // important: needs to be consumed
+            if (asyncResult.succeeded()) {
+                System.out.println("passed vertxclient");
+                assertEquals(MyEnum.value0, asyncResult.result().getEnum());
+            } else {
+                System.out.println("Cound not reach server: " + asyncResult.cause().getMessage());
+            }
+
+        });
+
+
+    }
+
 }
